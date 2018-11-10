47b49dfa03a3f34851221b30684b908e17138952
Alex Powell <alexander.powell@gmail.com>
2018-11-10 20:51:41 +0000

    Adding AudioKit and separate host app

---
+++ Abbey Road/Abbey Road Host/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Abbey Road Host
+//
+//  Created by Alex Powell on 11/10/18.
+//  Copyright © 2018 Nightshade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
