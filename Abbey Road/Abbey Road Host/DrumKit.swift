960ddd2bee0adb096ca303f86960142628403287
P L Garlaschi Sousa <plgarlaschisousa@Ps-MacBook-Pro.local>
2018-11-10 21:59:08 +0000

    DrumKitClass

---
+++ Abbey Road/Abbey Road Host/DrumKit.swift
@@ -0,0 +1,48 @@
+//
+//  DrumKit.swift
+//  Abbey Road Host
+//
+//  Created by P L Garlaschi Sousa on 10/11/2018.
+//  Copyright © 2018 Nightshade. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class DrumKit {
+    static let sharedInstance = DrumKit()
+    let drums = AKAppleSampler()
+    
+    init() {
+        
+        AudioKit.output = drums
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+        do {
+            let bassDrumFile = try AKAudioFile(readFileName: "Drums/bass_drum_C1.wav")
+            let clapFile = try AKAudioFile(readFileName: "Drums/clap_D#1.wav")
+            let closedHiHatFile = try AKAudioFile(readFileName: "Drums/closed_hi_hat_F#1.wav")
+            let hiTomFile = try AKAudioFile(readFileName: "Drums/hi_tom_D2.wav")
+            let loTomFile = try AKAudioFile(readFileName: "Drums/lo_tom_F1.wav")
+            let midTomFile = try AKAudioFile(readFileName: "Drums/mid_tom_B1.wav")
+            let openHiHatFile = try AKAudioFile(readFileName: "Drums/open_hi_hat_A#1.wav")
+            let snareDrumFile = try AKAudioFile(readFileName: "Drums/snare_D1.wav")
+            
+            try drums.loadAudioFiles([bassDrumFile,
+                                      clapFile,
+                                      closedHiHatFile,
+                                      hiTomFile,
+                                      loTomFile,
+                                      midTomFile,
+                                      openHiHatFile,
+                                      snareDrumFile])
+            
+        } catch {
+            AKLog("Files Didn't Load")
+        }
+        
+    }
+}
--- Abbey Road/Abbey Road Host/ViewController.swift
+++ Abbey Road/Abbey Road Host/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import AudioKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIGestureRecognizerDelegate {
 
+    let kit = DrumKit()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let tapGesture = UITapGestureRecognizer(target: self , action: #selector(Handler));
+        tapGesture.delegate = self
+        self.view.addGestureRecognizer(tapGesture);
     }
-
+    
+    @objc func Handler(sender: UITapGestureRecognizer? = nil) {
+        
+        let x : CGFloat = (sender?.location(in: view).x)!;
+        let w : CGFloat = UIScreen.main.bounds.width;
+        if(x < w * 0.1){
+            try? self.kit.drums.play(noteNumber: 36 - 12)
+        }
+        else if(x < w * 0.2){
+            try? self.kit.drums.play(noteNumber: 38 - 12)
+        }
+        else if(x < w * 0.3){
+            try? self.kit.drums.play(noteNumber: 42 - 12)
+        }
+        else if(x < w * 0.4){
+            try? self.kit.drums.play(noteNumber: 46 - 12)
+        }
+        else if(x < w * 0.5){
+            try? self.kit.drums.play(noteNumber: 41 - 12)
+        }
+        else if(x < w * 0.6){
+            try? self.kit.drums.play(noteNumber: 47 - 12)
+        }
+        else if(x < w * 0.7){
+            try? self.kit.drums.play(noteNumber: 50 - 12)
+        }
+        else if(x < w * 0.8){
+            try? self.kit.drums.play(noteNumber: 39 - 12)
+        }
+        else if(x < UIScreen.main.bounds.width * 0.9){
+            try? self.kit.drums.play(noteNumber: 36 - 12)
+        }
+    }
+    
 
 }
 
